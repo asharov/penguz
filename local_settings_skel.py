5f443c68838f67b576c535ae3e02f4b02c848595
Jaakko Kangasharju <ashar+penguz@iki.fi>
2012-04-27 00:36:15 +0200

    Add settings file, sanitized from local settings

---
+++ local_settings_skel.py
@@ -0,0 +1,7 @@
+# Copy this file to local_settings.py and add any necessary local
+# settings here. You will want to set at least the DATABASES and
+# SECRET_KEY settings, and probably some others too.
+
+# Add only the settings you want to change from the defaults in
+# settings.py. Anything in this file will override any setting from
+# settings.py.
