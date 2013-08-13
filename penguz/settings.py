96c702e11e0ca488951efbfcf50d348cf915356a
Jaakko Kangasharju <ashar+penguz@iki.fi>
2013-08-13 02:34:14 +0200

    Fiddle with password hashers

--- penguz/settings.py
+++ penguz/settings.py
@@ -133,9 +133,10 @@ AUTHENTICATION_BACKENDS = (
 )
 
 PASSWORD_HASHERS = (
-    'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
     'django.contrib.auth.hashers.SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
     'django.contrib.auth.hashers.CryptPasswordHasher',
